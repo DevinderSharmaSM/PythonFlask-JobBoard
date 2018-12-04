92c822270f000ee48c7577eeab8348c2e1532d79
DevinderSharmaSM <45565934+DevinderSharmaSM@users.noreply.github.com>
2018-12-04 10:10:14 +0000

    Completed this module

--- jobs/app.py
+++ jobs/app.py
@@ -7,7 +7,7 @@ PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
 def open_connection():
-    connection = getattr(g, "-connection", None)
+    connection = getattr(g, "_connection", None)
 
     if connection == None:
         connection = g._connection = sqlite3.connect(PATH)
